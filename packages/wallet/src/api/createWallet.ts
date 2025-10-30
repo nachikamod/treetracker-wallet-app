607b3f32fa9ab34e932bd561587bd72698947c64
Nachiket Kamod <nkamod@uccs.edu>
2025-10-29 19:46:19 -0500

    chore: add test comment in createWallet function to trigger PR

--- packages/wallet/src/api/createWallet.ts
+++ packages/wallet/src/api/createWallet.ts
@@ -2,6 +2,7 @@ import axios, { isAxiosError } from "axios";
 import { Wallet } from "../types/wallet";
 import { TREETRACKER_API, WALLET_API_KEY } from "../utils/config";
 
+// Test Change to trigger pr
 async function createWallet(walletData: Wallet, token: string) {
   try {
     const response = await axios.post(
